6ce3780db4d9fd7b6d5a16ad3d4100209c3e3d24
justinjeong <justin.jeong5@gmail.com>
2020-12-09 20:27:04 +0900

    add: initial commit for mongodb

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://justinjeong:<password>@general.nbo0d.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => {
+  console.log('...database connected successfully!')
+}).catch((error) => {
+  console.error(error)
+})
+
+
 
 app.get('/', (req, res) => {
   res.send('connected server-side app')

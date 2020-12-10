ff7003bcca491e925b8a7e010c3cf06e58229f8c
justinjeong <justin.jeong5@gmail.com>
2020-12-10 13:28:14 +0900

    fix: reorder import

--- client/src/index.js
+++ client/src/index.js
@@ -7,12 +7,9 @@ import { applyMiddleware, createStore } from 'redux';
 import promiseMiddleware from 'redux-promise';
 import ReduxThunk from 'redux-thunk'
 import Reducer from './_reducers'
-
+import 'antd/dist/antd.css'
 
 const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
-
-
-import 'antd/dist/antd.css'
 
 ReactDOM.render(
   <Provider
@@ -24,8 +21,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

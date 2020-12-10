071a5f4bc3c76b2e17c7ae5ef614a391fa7cfa20
justinjeong <justin.jeong5@gmail.com>
2020-12-10 09:14:03 +0900

    add(db config): conseal mongoURI for security

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const bodyParser = require('body-parser');
+const config = require('./config/key')
 const { User } = require('./models/User');
 
 
@@ -9,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://justinjeong:<password>@general.nbo0d.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => {
   console.log('...database connected successfully!')

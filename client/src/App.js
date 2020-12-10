ddfb73ed741621a536400410932a936492c310e1
justinjeong <justin.jeong5@gmail.com>
2020-12-10 12:01:59 +0900

    feat: router using react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,25 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
+
+import LandingPage from './components/views/LandingPage/LandingPage'
+import RegisterPage from './components/views/RegisterPage/RegisterPage'
+import LoginPage from './components/views/LoginPage/LoginPage'
+
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/register" component={RegisterPage} />
+          <Route exact path="/login" component={LoginPage} />
+        </Switch>
+      </Router>
     </div>
   );
 }

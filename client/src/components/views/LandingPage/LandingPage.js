8c412465ae22ba8bce7fe6efc9cb41c3e1222505
justinjeong <justin.jeong5@gmail.com>
2020-12-12 00:28:10 +0900

    fix: showing logout btn according to auth

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -20,7 +20,7 @@ function LandingPage(props) {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh' }}>
       <h2>시작 페이지</h2>
-      {userAuthentication && <button onClick={handleLogout}>로그아웃</button>}
+      {userAuthentication && userAuthentication.isAuth && <button onClick={handleLogout}>로그아웃</button>}
     </div>
   )
 }

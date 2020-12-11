c8803ffc293383d6bb1d92c5a844bfe57f5062b7
justinjeong <justin.jeong5@gmail.com>
2020-12-11 16:34:50 +0900

    add(server): http status code for detail

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,9 +1,76 @@
 import React from 'react'
+import { Form, Input, Button, Checkbox } from 'antd';
+
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+};
+const tailLayout = {
+  wrapperCol: { offset: 8, span: 16 },
+};
+
 
 function LoginPage() {
+  const [form] = Form.useForm();
+
+  const initialValues = () => {
+    return {
+      remember: true,
+      email: localStorage.getItem('rememberMe'),
+    }
+  }
+
+  const onFinish = (values) => {
+    localStorage.setItem('rememberMe', values.email);
+
+
+  };
+
+  const onFinishFailed = ({ errorFields }) => {
+    // console.log(errorFields);
+    form.scrollToField(errorFields[0].name);
+  };
+
   return (
-    <div>
-      LoginPage
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh' }}>
+      <Form
+        {...layout}
+        name="basic"
+        style={{ width: '400px' }}
+        initialValues={initialValues()}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+      >
+        <Form.Item
+          label="이메일"
+          name="email"
+          rules={[{ required: true, message: '이메일을 입력해주세요.' },
+          { type: "email", message: '이메일의 형식이 올바르지 않습니다.' }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="비밀번호"
+          name="password"
+          rules={[{ required: true, message: '비밀번호를 입력해주세요.' },
+          { type: "string", message: '비밀번호의 형식이 올바르지 않습니다.' },
+          { whitespace: false, message: '비밀번호의 형식이 올바르지 않습니다.' },
+          { min: 6, message: '비밀번호는 6글자보다 길어야합니다.' }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+          <Checkbox>아이디 기억하기</Checkbox>
+        </Form.Item>
+
+        <Form.Item {...tailLayout}>
+          <Button type="primary" htmlType="submit" >
+            로그인
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   )
 }
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+
 const port = 5000;
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
@@ -16,51 +17,44 @@ mongoose.connect(config.mongoURI, {
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => {
   console.log('...database connected successfully!')
-}).catch((err) => {
-  console.error(err)
+}).catch((error) => {
+  console.error(error)
 })
 
 
 
 app.get('/api', (req, res) => {
-  res.send('connected server-side app')
+  res.status(200).send('connected server-side app')
 })
 
 app.post('/api/user/register', (req, res) => {
   const user = new User(req.body);
 
-  user.save((err, doc) => {
-    if (err) return res.json({ success: false, err })
+  user.save((error, doc) => {
+    if (error) return res.status(400).json({ success: false, error })
     return res.status(200).json({ success: true })
   })
 })
 
 app.post('/api/user/login', (req, res) => {
 
-  User.findOne({ email: req.body.email }, (err, user) => {
-    if (err) return res.json({ success: false, err })
+  User.findOne({ email: req.body.email }, (error, user) => {
+    if (error) return res.status(400).json({ success: false, error })
     if (!user) {
-      return res.json({
-        loginSuccess: false,
-        message: '존재하지 않는 사용자입니다.'
-      });
+      return res.status(400).send('존재하지 않는 사용자입니다.');
     }
 
-    user.comparePassword(req.body.password, (err, isMatch) => {
-      if (err) return res.json({ success: false, err })
+    user.comparePassword(req.body.password, (error, isMatch) => {
+      if (error) return res.status(400).json({ success: false, error })
       if (!isMatch) {
-        return res.json({
-          loginSuccess: false,
-          message: '비밀번호가 일치하지 않습니다.'
-        });
+        return res.status(400).send('비밀번호가 일치하지 않습니다.');
       }
-      user.generateToken((err, user) => {
-        if (err) return res.status(400).send(err);
+      user.generateToken((error, user) => {
+        if (error) return res.status(400).send(error);
 
         // cookie에 저장
-        res.cookie('x_auth', user.token)
-          .status(200)
-          .json({ loginSuccess: true, userId: user._id });
+        res.cookie('x_auth', user.token).status(200)
+          .json({ success: true, userId: user._id });
       })
     })
   })
@@ -92,8 +86,8 @@ app.get('/api/user/logout', auth, (req, res) => {
 
   User.findOneAndUpdate({ _id: req.user._id, },
     { token: '' },
-    (err, user) => {
-      if (err) return res.json({ success: false, err })
+    (error, user) => {
+      if (error) return res.status(400).json({ success: false, error })
       res.status(200).json({ success: true })
     })
 })

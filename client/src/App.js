f51a3f7c4207f9fe9928042c3cc1c6848021572b
justinjeong <justin.jeong5@gmail.com>
2020-12-13 18:59:16 +0900

    feat: loadingPage using suspense&react-loading

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -10,18 +10,21 @@ import LandingPage from './components/views/LandingPage/LandingPage'
 import RegisterPage from './components/views/RegisterPage/RegisterPage'
 import LoginPage from './components/views/LoginPage/LoginPage'
 import Auth from './hoc/auth'
+import LoadingPage from './components/views/LoadingPage/LodingPage';
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Auth(LandingPage, null)} />
-          <Route exact path="/register" component={Auth(RegisterPage, false)} />
-          <Route exact path="/login" component={Auth(LoginPage, false)} />
-        </Switch>
-      </Router>
-    </div>
+    <Suspense fallback={<LoadingPage />}>
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Auth(LandingPage, null)} />
+            <Route exact path="/register" component={Auth(RegisterPage, false)} />
+            <Route exact path="/login" component={Auth(LoginPage, false)} />
+          </Switch>
+        </Router>
+      </div>
+    </Suspense>
   );
 }
 

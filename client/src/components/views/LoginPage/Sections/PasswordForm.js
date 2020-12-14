78eff9d79143e02e9b3b38f0cbeaa46f13faa9c6
justinjeong <justin.jeong5@gmail.com>
2020-12-14 20:27:52 +0900

    refactor(component): PasswordForm detached from LoginPage

--- client/src/components/views/LoginPage/Sections/PasswordForm.js
+++ client/src/components/views/LoginPage/Sections/PasswordForm.js
@@ -1,85 +1,39 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { withRouter } from 'react-router-dom';
-import { Form, Button, Checkbox } from 'antd';
-import { LOGIN_USER_REQUEST } from '../../../_sagas/types';
-import { Link } from 'react-router-dom';
-import LoadingPage from '../LoadingPage/LoadingPage';
-import PasswordForm from './Sections/PasswordForm';
-import EmailForm from './Sections/EmailForm';
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Form, Input, } from 'antd'
+import { LockOutlined } from '@ant-design/icons';
 
-const layout = {
-  labelCol: { span: 8 },
-  wrapperCol: { span: 16 },
-};
-const tailLayout = {
-  wrapperCol: { offset: 8, span: 16 },
-};
+function PasswordForm() {
 
-function LoginPage(props) {
-  const [form] = Form.useForm();
-  const dispatch = useDispatch();
-  const { loginUserLoading, loginUserDone, loginUserError } = useSelector(state => state.user)
-
-  useEffect(() => {
-    if (loginUserDone) {
-      props.history.push('/')
-    }
-  }, [loginUserDone])
-
-  const initialValues = () => {
-    return {
-      remember: true,
-      email: localStorage.getItem('rememberMe'),
-    }
-  }
-
-  const onFinish = (values) => {
-    localStorage.setItem('rememberMe', values.email);
-    dispatch({
-      type: LOGIN_USER_REQUEST,
-      payload: {
-        email: values.email,
-        password: values.password
-      }
-    })
-  };
-
-  const onFinishFailed = ({ errorFields }) => {
-    form.scrollToField(errorFields[0].name);
-  };
+  const { loginUserError } = useSelector(state => state.user)
 
   return (
-    <>
-      {loginUserLoading && <LoadingPage />}
-      {!loginUserLoading &&
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh' }}>
-          <Form
-            {...layout}
-            name="normal_login"
-            className="login-form"
-            style={{ width: '400px' }}
-            initialValues={initialValues()}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-          >
-            <EmailForm />
-            <PasswordForm />
-            <Form.Item {...tailLayout} name="remember" valuePropName="checked">
-              <Checkbox>아이디 기억하기</Checkbox>
-            </Form.Item>
-
-            <Form.Item {...tailLayout}>
-              <Button type="primary" htmlType="submit" className="login-form-button" style={{ width: '100%' }}>
-                로그인
-              </Button>
-              <Link to='/register'>아직 회원이 아니시라면</Link>
-            </Form.Item>
-          </Form>
-        </div >
+    <div>
+      {(!loginUserError || loginUserError.code === 'NoSuchUser') && <Form.Item
+        label="비밀번호"
+        name="password"
+        rules={[{ required: true, message: '비밀번호를 입력해주세요.' },
+        { type: "string", message: '비밀번호의 형식이 올바르지 않습니다.' },
+        { whitespace: false, message: '비밀번호의 형식이 올바르지 않습니다.' },
+        { min: 6, message: '비밀번호는 6글자보다 길어야합니다.' }]}
+      >
+        <Input.Password prefix={<LockOutlined className="site-form-item-icon" />} placeholder="password" />
+      </Form.Item>}
+      {loginUserError && loginUserError.code === 'PasswordMismatch' &&
+        <Form.Item
+          label="비밀번호"
+          validateStatus="error"
+          name="password"
+          rules={[{ required: true, message: '비밀번호를 입력해주세요.' },
+          { type: "string", message: '비밀번호의 형식이 올바르지 않습니다.' },
+          { whitespace: false, message: '비밀번호의 형식이 올바르지 않습니다.' },
+          { min: 6, message: '비밀번호는 6글자보다 길어야합니다.' }]}
+        >
+          <Input.Password prefix={<LockOutlined className="site-form-item-icon" />} placeholder="비밀번호가 일치하지 않습니다." />
+        </Form.Item>
       }
-    </>
+    </div>
   )
 }
 
-export default withRouter(LoginPage);
+export default PasswordForm
